b8a935f3aeaa92d466701bdf858d229671b99a6c
theonly8z <888888zombies@gmail.com>
2025-03-27 19:01:01 -0400

    wow

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -875,12 +875,13 @@
 #define TRAINER_FLASH_HIKER                 869
 #define TRAINER_CAMEO_ETHAN                 870
 #define TRAINER_CAMEO_COLLIN                871
+#define TRAINER_CAMEO_NELLIE                872
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
 
-#define TRAINERS_COUNT                      872
+#define TRAINERS_COUNT                      873
 #define MAX_TRAINERS_COUNT                  900 // 864
 #define TRAINER_PARTNER(partner)           (MAX_TRAINERS_COUNT + partner)
 
